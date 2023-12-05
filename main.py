7f6964a62c79460c3e6ae22ca779d94b1313974f
Enzo Sandré <atelier@enzosandre.fr>
2023-12-05 11:39:52 +0100

    Prototype incomplet

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+LANG = 'FR'
+TIME_IN_HOURS = 8
+
+def dire_bonjour():
+    if LANG == 'FR':
+        return "Bonjour"
+    else:
+        return "Hello"
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(dire_bonjour())
+    phrase = input('Entrez une phrase :')
+    inversé = phrase[::-1]
+
+    print(inversé)
+
+    if phrase == inversé:
+        print('Bien dit')
+
+    print('Au revoir')
